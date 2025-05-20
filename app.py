ce9daae799e64dac09e1761f3c26833b1002853e
Maxi Arrieta <arrieta.maxi@gmail.com>
2025-05-19 22:24:58 -0300

    app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect('grocery_list.db')
         cursor = db.cursor()
-        cursor.execute("select * from groceries")
+        cursor.execute("select name from groceries")
     return cursor.fetchall()
 
 @app.teardown_appcontext
